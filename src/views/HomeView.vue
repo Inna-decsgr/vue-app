898daad8209dc79890e48dff70c6e487e71dae87
Inna-decsgr <anjgkwl27@gmail.com>
2024-07-28 23:25:01 +0900

    Add popular movies section with API integration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,40 @@
 <template>
   <div>
-    CinemaInsights
+    <h1>인기 영화</h1>
+    <ul>
+      <li v-for="movie in popularMovies" :key="movie._id">
+        <h2>{{ movie.title }}</h2>
+        <img :src="movie.poster_url" :alt="movie.title">
+        <p>{{ movie.description }}</p>
+        <p>Director: {{ movie.director }}</p>
+        <p>Cast: {{ movie.cast.join(', ') }}</p>
+        <p>Release Date: {{ movie.release_date }}</p>
+        <p>Rating: {{ movie.rating }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+
 export default {
-}
+  data() {
+    return {
+      popularMovies: [],
+    };
+  },
+  mounted() {
+    this.fetchPopularMovies();
+  },
+  methods: {
+    async fetchPopularMovies() {
+      try {
+        const response = await this.$http.get('http://localhost:5000/api/movies/popular');
+        this.popularMovies = response.data;
+      } catch (error) {
+        console.error('Error fetching popular movies:', error);
+      }
+    },
+  },
+};
 </script>
