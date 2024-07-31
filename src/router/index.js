1b0ddf39fdcbbcfc1c15af6b3b95887e0050b296
Inna-decsgr <anjgkwl27@gmail.com>
2024-08-01 02:39:33 +0900

    Refactor Vue router configuration for genre-based movie views

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginPage from '../views/LoginPage.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import LatestMovies from '../views/LatestMovies.vue';
+import GenreMovies from '../views/GenreMovies.vue';
+import GenreOverview from '../views/GenreOverview.vue';
+
 
 const routes = [
   {
@@ -9,15 +12,23 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: LoginPage
-  }
-]
+    path: '/latest',
+    name: 'LatestMovies',
+    component: LatestMovies
+  },
+  {
+    path: '/genre',
+    component: GenreOverview, // 기본 페이지
+    redirect: '/genre/action', // 기본 리다이렉트
+    children: [
+      { path: ':genreName', name: 'GenreMovies', component: GenreMovies }
+    ]
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
