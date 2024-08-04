adbe69672c9a332ee40e8e8e69dbffa61344a114
Inna-decsgr <anjgkwl27@gmail.com>
2024-08-04 23:48:04 +0900

    Add search input component with event handling

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input type="text" v-model="query" @input="onChange" placeholder="검색어를 입력하세요" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      query: ''
+    };
+  },
+  methods: {
+    onChange() {
+      if (this.query.trim()) { // 검색어가 비어 있지 않은 경우에만 검색 이벤트를 발생
+        this.$emit('search', this.query);
+      } else {
+        // 검색어가 비어 있는 경우, 검색 이벤트를 발생시키지 않음
+        this.$emit('search', null);
+      }
+    }
+  }
+};
+</script>
+
