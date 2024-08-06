9f5184cdce9a4326b96376ee1df246dd6c37ae74
Inna-decsgr <anjgkwl27@gmail.com>
2024-08-06 22:52:54 +0900

    Enhance movie list layout with Bootstrap cards

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,16 +1,22 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="movie in movies" :key="movie._id">
-        <h2>{{ movie.title }}</h2>
-        <img :src="movie.poster_url" :alt="movie.title" />
-        <p>{{ movie.description }}</p>
-        <p>Director: {{ movie.director }}</p>
-        <p>Cast: {{ movie.cast.join(', ') }}</p>
-        <p>Release Date: {{ movie.release_date }}</p>
-        <p>Rating: {{ movie.rating }}</p>
-      </li>
-    </ul>
+  <div class="container mt-5">
+    <div class="row">
+      <div v-for="movie in movies" :key="movie._id" class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-5">
+        <div class="card h-100">
+          <img :src="movie.poster_url" :alt="movie.title" class="card-img-top" />
+          <div class="card-body">
+            <h5 class="card-title fw-bold">{{ movie.title }}</h5>
+            <p class="card-text description">{{ movie.description }}</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><span class="fw-bold">감독: </span>{{ movie.director }}</li>
+            <li class="list-group-item"><span class="fw-bold">출연진:</span> {{ movie.cast.join(', ') }}</li>
+            <li class="list-group-item"><span class="fw-bold">개봉 날짜:</span> {{ movie.release_date }}</li>
+            <li class="list-group-item"><span class="fw-bold">평점:</span> {{ movie.rating }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,3 +30,24 @@ export default {
   }
 }
 </script>
+
+<style>
+.container {
+  max-width: 100%;
+}
+
+.card-img-top {
+  width: 100%; 
+  height: 500px; 
+  object-fit: cover; 
+}
+
+.description {
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-height: 3em;
+}
+</style>
